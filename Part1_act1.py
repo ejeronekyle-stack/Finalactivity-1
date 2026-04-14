1faf8955a50f5a016adeedeafdeaa438b09fa36b
Jerone Kyle Esto <jbesto@paterostechnologicalcollege.edu.ph>
2026-04-15 01:07:30 +0800

    Part1_act1.py

---
+++ Part1_act1.py
@@ -0,0 +1,17 @@
+# Part 1: Identify & Create (Warm-up)
+
+# 1. Tuple of 5 favorite fruits
+fruits = ("apple", "banana", "mango", "grapes", "orange")
+print("Tuple of Fruits:", fruits)
+
+# 2. List of 5 daily tasks
+tasks = ["study", "exercise", "cook", "clean", "sleep"]
+print("List of Tasks:", tasks)
+
+# 3. Set of unique numbers from [1, 2, 2, 3, 4, 4, 5]
+numbers = {1, 2, 2, 3, 4, 4, 5}
+print("Set of Unique Numbers:", numbers)
+
+# 4. Dictionary with keys: "name", "age", "course"
+student = {"name": "Jerone", "age": 20, "course": "BSIT"}
+print("Dictionary:", student)
